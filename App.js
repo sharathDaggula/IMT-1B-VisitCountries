fb89b8752bccf1db12d780bce078153c2fc96313
sharathDaggula <157371072+sharathDaggula@users.noreply.github.com>
2025-08-17 16:45:20 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,149 @@
+import './App.css'
+import {Component} from 'react'
+
+const initialCountriesList = [
+  {
+    id: '53c9c67a-c923-4927-8a75-fdfc4bc5ec61',
+    name: 'Australia',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-australia-img.png',
+    isVisited: false,
+  },
+  {
+    id: '8baa8029-fb2c-4f06-bfcc-3dc9ad12b24d',
+    name: 'Canada',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-canada-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1b520f98-6548-41f3-816e-c8b887865172',
+    name: 'Greenland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-greenland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '25841996-fbfd-4554-add4-4c94082c8ccd',
+    name: 'India',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-india-img.png',
+    isVisited: true,
+  },
+  {
+    id: '603c3568-13b0-11ec-82a8-0242ac130003',
+    name: 'Netherlands',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-netherland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '3c988dec-55e1-477d-a9e2-b354fd559849',
+    name: 'Portugal',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-portugal-img.png',
+    isVisited: false,
+  },
+  {
+    id: 'd766f754-34f7-413e-81ec-9992821b97fa',
+    name: 'Switzerland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-switzerland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '7ebb4e04-b124-417f-a69e-564a456d70f1',
+    name: 'Thailand',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-thailand-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1e4b1dcd-6ace-4dde-ad8d-675927d5ae47',
+    name: 'United Kingdom',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-united-kingdom-img.png',
+    isVisited: true,
+  },
+  {
+    id: 'e76da8ca-bc48-4981-902b-a4d2d46feb6d',
+    name: 'Venezuela',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-venezuela-img.png',
+    isVisited: false,
+  },
+]
+
+class App extends Component {
+  state = {
+    countriesList: initialCountriesList,
+  }
+
+  toggleVisit = id =>
+    this.setState(prevState => ({
+      countriesList: prevState.countriesList.map(each =>
+        each.id === id ? {...each, isVisited: !each.isVisited} : each,
+      ),
+    }))
+
+  render() {
+    const {countriesList} = this.state
+    const visitCountries = countriesList
+    const visitedCountries = countriesList.filter(each => each.isVisited)
+    return (
+      <div className="bg-container">
+        <h1>Countries</h1>
+        <ul className="visit-countries-container">
+          {visitCountries.map(each => (
+            <li key={each.id}>
+              <div className="visit-country-item-container">
+                <p>{each.name}</p>
+                {each.isVisited ? (
+                  <p>visited</p>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => this.toggleVisit(each.id)}
+                  >
+                    visit
+                  </button>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <h1>Visited Countries</h1>
+        <div className="visited-countries-container">
+          {visitedCountries.length > 0 ? (
+            <ul className="bottom-container">
+              {visitedCountries.map(each => (
+                <li key={each.id}>
+                  <div className="visited-country-item-container">
+                    <img
+                      src={each.imageUrl}
+                      alt="thumbnail"
+                      className="visited-country-image"
+                    />
+                    <div className="name-and-button-container">
+                      <p>{each.name}</p>
+                      <button
+                        type="button"
+                        onClick={() => this.toggleVisit(each.id)}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No Countries Visited Yet!</p>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
